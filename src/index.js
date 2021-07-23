43aa392411b4c094956bd87c6ba3f49060e57f95
hleppps <ddddffffgggg36@gmail.com>
2021-07-23 20:57:20 +0300

    v1

--- src/index.js
+++ src/index.js
@@ -2,11 +2,8 @@ import './styles/style.less'
 import Parallax from 'parallax-js'
 
 $(document).ready(function() {
-  $('.parallax__list>li').addClass('layer')
-  let scene1 = $('.parallax__list')
-  let scene = document.querySelector('.parallax__list')
-  console.log(scene1)
-  console.log(scene)
+  $('.wrapper').addClass('active')
+  let scene = $('.parallax__list')[0]
 
   let parallaxInstance = new Parallax(scene)
   parallaxInstance.friction(0.2, 0.2);
